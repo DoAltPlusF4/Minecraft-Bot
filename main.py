575c20cf62a020d04f08d2986cc89df2d89cfce5
Lenny P Critchley <doaltplusf4@gmail.com>
2020-07-06 11:06:20 +0100

    Deleted terraria bot. 🦀

--- main.py
+++ main.py
@@ -9,16 +9,10 @@ if __name__ == "__main__":
         command_prefix="m!",
         description="Now with added bullshit!"
     )
-    terraria_bot = terraria.bot.Bot(
-        command_prefix="t!",
-        description="Something here!"
-    )
 
     MC_TOKEN = os.environ.get("mc_token")
-    TERRARIA_TOKEN = os.environ.get("terraria_token")
 
     loop = asyncio.get_event_loop()
     mc_task = loop.create_task(mc_bot.start(MC_TOKEN))
-    terraria_task = loop.create_task(terraria_bot.start(TERRARIA_TOKEN))
-    gathered = asyncio.gather(mc_task, terraria_task, loop=loop)
+    gathered = asyncio.gather(mc_task, loop=loop)
     loop.run_until_complete(gathered)
